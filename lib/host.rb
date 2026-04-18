ac8dc949f55c10734ff3cce73f2f86afcef00fb6
Kipras Chi <kipraschi@gmail.com>
2026-04-18 18:42:11 +0300

    feat: add a placeholder for the host to provide feedback

--- lib/host.rb
+++ lib/host.rb
@@ -1,18 +1,25 @@
 class Host
   def initialize
     @secret = random_word
-    puts @secret
+    @placeholder = create_placeholder
   end
+
+  private
 
   def random_word 
     File.readlines('lib/dictionary.txt')
       .select { |word| within_limits?(word.strip, 5, 12) }
       .sample
       .strip
+      .chars
   end
 
   def within_limits?(word, lower, upper)
     word.length >= lower && word.length <= upper
+  end
+
+  def create_placeholder
+    Array.new(@secret.length, "_").join
   end
 
 end
